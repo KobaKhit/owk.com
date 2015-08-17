63ded966278e13ba23fa6b4d4f7131a32deccd9d
Kobakhit <kobakhit@gmail.com>
2015-08-17 03:04:45 +0200

    redesign

--- js/main.js
+++ js/main.js
@@ -1,20 +1,16 @@
+$(window).load(function(){
+	$("#landing-img")
+			.mouseover(function(){
+				$(this)
+				.velocity({ width: '+=1%'})
+			})
+			.mouseout(function(){
+				$(this)
+				.velocity({ width: '100px'})
+			})
 
-// On navbar click load .html file on same page
-function inc(a){
-	$('#includeContent').load('/' + a  + '.html')
-}
-
-$("#landing-img")
-		.mouseover(function(){
-			$(this)
-			.velocity({ width: '+=1%'})
-		})
-		.mouseout(function(){
-			$(this)
-			.velocity({ width: '100px'})
-		})
-
-    $(".nav li").on("click", function() {
-      $(".nav li").removeClass("active");
-      $(this).addClass("active");
-    });
+	    $(".nav li").on("click", function() {
+	      $(".nav li").removeClass("active");
+	      $(this).addClass("active");
+	    });
+});
